14f2b1492104bcf0b20c5c6e72ff80888902130a
Rustam <electroman232@gmail.com>
2020-01-30 09:27:21 +0300

    Initial commit. Installed RabbitMQ client and Newtonsoft.json

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace RabbitMQ
+﻿namespace RabbitMQ
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
