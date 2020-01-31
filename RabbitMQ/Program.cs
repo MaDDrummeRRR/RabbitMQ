c303f4891a2099ff86cb86dfcd9c6d5d9fca5e43
Rustam <electroman232@gmail.com>
2020-01-31 08:09:42 +0300

    Made another example. Made class, serialize it and load to RabbitMQ

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Text;
 
@@ -35,13 +36,38 @@ namespace RabbitMQ
                 Console.WriteLine("Published message: " + message);
             }
 
+            //---------------------------------------Messages with Class------------------------------------------------------------------------------------------
+
+            for (var i = 0; i < 5; i++)
+            {
+                var message = new MyMessage            // Create text message
+                {
+                    Address = "Moscow, Russia",
+                    ShoeSize = 45,
+                    Name = "Rustam"
+                };
+
+                var messageBodyString = JsonConvert.SerializeObject(message);                               // Serializing message to JSON format with JsonConvert
+                var messageBodyBytes = Encoding.UTF8.GetBytes(messageBodyString);                           // Encode messageBodyString to byte array in UTF8 format
+
+                channel.BasicPublish("RabbitMQExchangeTest", "routing key", null, messageBodyBytes);        // Publish message to queue with array of bytes
+
+                Console.WriteLine("Published message: " + messageBodyString);
+            }
 
 
-
+            Console.ReadKey();
 
 
             channel.Dispose();                      // Release channel in that connection
             conn.Dispose();                         // Release resources!!!!!
         }
+    }
+
+    public class MyMessage
+    {
+        public string Name { get; set; }
+        public int ShoeSize { get; set; }
+        public string Address { get; set; }
     }
 }
